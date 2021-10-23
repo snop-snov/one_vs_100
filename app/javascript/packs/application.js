ce6ada3ee1f998a28cd2ee6c4b108b8999ac915a
Svetlana Saveleva <ssaweleva@gmail.com>
2021-10-23 19:41:31 +0400

    add materialize

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import './sass/materialize-css-custom';
+import './sass/_custom-sass';
+
+import M from 'materialize-css/dist/js/materialize'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
