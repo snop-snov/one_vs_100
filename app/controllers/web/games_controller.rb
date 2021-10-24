062d4df52fb52b51fb63e6a4687d71bda93023f4
Svetlana Saveleva <ssaweleva@gmail.com>
2021-10-24 21:29:24 +0400

    add game page

---
+++ app/controllers/web/games_controller.rb
@@ -0,0 +1,4 @@
+class Web::GamesController < Web::ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   scope module: :web do
     resource :session, only: %i[destroy]
+    resource :game, only: %i[show]
     resources :users, only: %i[] do
       scope module: :users do
         resources :cheerings, only: %i[create]
